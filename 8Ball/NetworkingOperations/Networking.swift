06cd8cb586422a9cf752c619d4b1b282d06738d9
Svyatoslav Chaplin <svyat12.93@gmail.com>
2019-09-02 20:23:38 +0300

    BugFix 1.0

---
+++ 8Ball/NetworkingOperations/Networking.swift
@@ -0,0 +1,33 @@
+//
+//  Networking.swift
+//  8Ball
+//
+//  Created by Svyat Chaplin on 9/1/19.
+//  Copyright © 2019 Svyat Chaplin. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class Networking {
+    
+    
+    // Получаем данные из сети
+    func getDataFromInternet(complitionHandler: @escaping (Data) -> Void) {
+        let session = URLSession.shared
+        let url = URL(string: "https://8ball.delegator.com/magic/JSON/question")!
+        session.dataTask(with: url) { (data, response, error) in
+            DispatchQueue.main.async {
+                complitionHandler(data!)
+            }
+        }.resume()
+    }
+    
+    // Расшифровуем данные при помощи модели
+    func prepareDataToUse(data: Data?) -> String {
+        let internetDict = try? JSONDecoder().decode(AnswerModel.self, from: data!)
+        let answerReadyToUse = internetDict?.magic.answer
+        return answerReadyToUse!
+    }
+    
+}
